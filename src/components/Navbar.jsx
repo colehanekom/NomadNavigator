1eb8e911778d949fd3abcb6271e33bdf8947b274
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-06 13:44:59 +0200

    Changed background of nav to transparent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/icon-logo.png';
 
 const Navbar = () => {
   return (
-    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-black'>
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-transparent'>
       <div>
         <img src={Logo} alt="Logo Image" style={{ width: '80px' }} />
       </div>

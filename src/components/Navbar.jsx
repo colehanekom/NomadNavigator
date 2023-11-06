6ee678ea95f2ea64c3e3caa4e91789cfa324ed0e
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-06 15:04:01 +0200

    Increased padding on x-axis and removed margin left

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import Logo from '../assets/icon-logo.png';
 const Navbar = () => {
   return (
     <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-transparent'>
-      <div>
+      <div className='sm:flex px-10'>
         <img src={Logo} alt="Logo Image" style={{ width: '80px' }} />
       </div>
-      <ul className='sm:flex px-4'>
+      <ul className='sm:flex px-10'>
         <li>
           <a href='#signup'>Sign Up</a>
         </li>
-        <li style={{ marginLeft: '10px' }}>
+        <li >
           <a href='#signin'>Sign In</a>
         </li>
       </ul>

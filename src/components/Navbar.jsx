913e686f32f839a20c71970e31ec307a91606806
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-06 13:31:34 +0200

    Added logo to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../assets/icon-logo.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-black'>
+      <div>
+        <img src={Logo} alt="Logo Image" style={{ width: '80px' }} />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;

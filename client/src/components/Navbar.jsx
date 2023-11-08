a7c6877889d6df99fcb9660051771cac18b1b508
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-08 14:46:52 +0200

    Added margin left to logo and added tailwind css code for width

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-transparent'>
-    <div className='sm:flex '>
-      <img src={Logo} alt="Logo Image" style={{ width: '80px' }} />
+    <div className='sm:flex'>
+      <img src={Logo} alt="Logo Image" className='w-[80px] lg:ml-8'/>
     </div>
 
     <ul className='flex px-10 space-x-4'>

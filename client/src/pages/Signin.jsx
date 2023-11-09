1f7f3b6c97f580801a981e6d18a9649e6318b200
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-09 15:01:31 +0200

    Updated Signin.jsx

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='w-full h-[100vh] bg-black flex items-center justify-center p-6'>
+      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-slate-50 rounded-xl overflow-hidden shadow-xl '>
+        {/* LEFT */}
+       
+        {/* RIGHT */}
+        <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>  
+          <div className='w-full flex gap-2 items-center mb-6'>
+            <span className='text-4xl text-[#1065A1] font-semibold'>Nomad Navigator</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

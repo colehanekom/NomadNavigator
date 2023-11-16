c5c2f002fb3e18668934007b7ba6ddc1497cccea
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-16 15:33:51 +0200

    Added Google logo svg and text in between buttons

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import Logo from '../assets/nomad-navigator-logo.png';
 import { FaGoogle } from 'react-icons/fa'; 
+import googleLogo from '../assets/googlelogo.svg';
+
 
 
 const Signup = () => {
@@ -33,17 +35,27 @@ const Signup = () => {
         {/* RIGHT */}
         <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>  
           <div className='w-full flex gap-2 items-center mb-6'>
-            <span className='text-4xl font-semibold'>Sign Up</span>
+            <span className='text-4xl font-semibold mb-10'>Sign Up</span>
           </div>
           <form className="py-8 flex flex-col gap-5" 
           onSubmit={handleSubmit(onSubmit)}>
          
-            <button className='inline-flex justify-center items-center rounded-full bg-white px-8 py-3 text-sm font-medium text-black outline hover:scale-105'>
-            <FaGoogle className="mr-2" style={{ color: '#DB4437' }} /> 
-            <Link to='/google'>Sign up with Google</Link>
-          </button>
+         <button className='inline-flex justify-center items-center rounded-full bg-white px-8 py-3 text-sm font-medium text-black outline hover:scale-105'>
+          <Link to='/google'>
+            <div className="flex items-center">
+              <img src={googleLogo} alt="Google Logo" className="w-5 h-5 mr-2" /> 
+              Sign up with Google
+            </div>
+          </Link>
+        </button>
 
-             <button className='inline-flex justify-center rounded-full bg-[#1065A1] px-8 py-3 text-sm font-medium text-white outline-none hover:scale-105'>
+          <div className='m-10 grid grid-cols-3 items-center text-black'>
+            <hr className='border-black'/>
+            <p className='text-center'>OR</p>
+            <hr className='border-black'/>
+           </div>
+
+            <button className='inline-flex justify-center rounded-full bg-[#1065A1] px-8 py-3 text-sm font-medium text-white outline-none hover:scale-105'>
               <Link to = '/email-signup'> Continue with email</Link>
             </button>
           </form>

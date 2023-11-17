4d4554e9af73eee3b0ac72956ef527fdb3d86d56
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-17 13:51:22 +0200

    Replaced outline with a border and border colour

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -40,7 +40,7 @@ const Signup = () => {
           <form className="py-8 flex flex-col gap-5" 
           onSubmit={handleSubmit(onSubmit)}>
          
-         <button className='inline-flex justify-center items-center rounded-full bg-white px-8 py-3 text-sm font-medium text-black outline hover:scale-105'>
+         <button className='inline-flex justify-center items-center rounded-full bg-white px-8 py-3 text-sm font-medium text-black border border-gray-300 hover:scale-105'>
           <Link to='/google'>
             <div className="flex items-center">
               <img src={googleLogo} alt="Google Logo" className="w-5 h-5 mr-2" /> 

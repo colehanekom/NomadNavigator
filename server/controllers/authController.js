1ac036a42076dcbf62e8c3a60d62eb2f15c32de6
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-11-24 13:11:35 +0200

    Added login function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,3 +34,52 @@ export const register = async (req, res, next) => {
             res.status(404).json({message: error.message});
         }
 };
+
+export const login = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try{
+        //validation
+        if(!email || !password){
+            next("Please Provide User Credentials");
+            return;
+        }
+
+        //find user by email
+        const user = await Users.findOne({email}).select("+password").populate({
+        });
+
+        if(!user){
+            next("Invalid email or password");
+            return;
+        }
+
+        if(!user?.verified){
+            next("User email is not verified. Check your email acount and verify your email");
+            return;
+        }
+
+        //compare password
+        const isMatch = await compareString(password, user?.password);
+
+        if(!isMatch){
+            next("Invalid email or password");
+            return;
+        }
+
+            user.password = undefined;
+
+            const token = createJWT(user?._id);
+
+        res.status(201).json({
+            success: true,
+            message: "Login successfully",
+            user,
+            token,
+        });
+
+    }catch(error){
+        console.log(error);
+        res.status(404).json({message: error.message});
+    }
+};

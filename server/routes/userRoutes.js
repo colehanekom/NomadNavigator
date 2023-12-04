e200809193ebf9bda9b896a2bc40cd08d1957011
colehanekom <Coleh304@gmail.com>
2023-12-04 17:07:50 +0000

    Added route for reset password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,21 @@
 import express from "express";
 import path from "path";
-import { verifyEmail } from "../controllers/userContoller.js";
+import { changePassword, requestPasswordReset, resetpassword, verifyEmail } from "../controllers/userContoller.js";
 
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
 
 router.get("/verify/:userId/:token", verifyEmail);
+//Password Reset
+router.post("/request-passwordreset", requestPasswordReset);
+router.get("/reset-password/:userId/:token", resetpassword);
+router.post("/reset-password", changePassword);
 
 router.get("/verified", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/build", "index.html"));
+});
+
+router.get("/resetpassword", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/build", "index.html"));
 });
 

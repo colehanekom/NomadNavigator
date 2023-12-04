4375fe01db4b3c08b7cca4f0ce9c3d40d5898b8f
colehanekom <Coleh304@gmail.com>
2023-12-04 17:07:20 +0000

    Added code for reset password

--- server/controllers/userContoller.js
+++ server/controllers/userContoller.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import Verification from "../models/emailVerification.js";
-import { compareString } from "../utils/index.js";
+import { compareString, hashString } from "../utils/index.js";
 import Users from "../models/userModel.js";
+import PasswordReset from "../models/passwordReset.js";
+import { resetPasswordLink } from "../utils/sendEmail.js";
 
 export const verifyEmail = async (req, res) => {
     const{userId, token} = req.params;
@@ -64,5 +66,99 @@ export const verifyEmail = async (req, res) => {
     }catch(error) {
         console.log(err);
         res.redirect(`/users/verified?message=`);
+    }
+};
+
+export const requestPasswordReset = async (req, res) => {
+    try {
+        const {email} = req.body;
+
+        const user = await Users.findOne({email});
+
+        if(!user){
+            return res.status(404).json({
+                status: "FAILED",
+                message: "Email address not found.",
+            });
+        }
+
+        const existingRequest = await PasswordReset.findOne({email});
+        if(existingRequest) {
+            if(existingRequest.expiresAt > Date.now()) {
+                return res.status(201).json({
+                    status: "PENDING",
+                    message: "Reset password link has been sent to your email.",
+                });
+            }
+            await PasswordReset.findOneAndDelete({email});
+        }
+        await resetPasswordLink(user, res);
+    } catch (error) {
+        console.error(error);
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const resetpassword = async (req, res) => {
+    const {userId, token} = req.params;
+
+    try {
+        //Find record
+        const user = await Users.findById(userId);
+
+        if(!user){
+            const message = "Invalid password reset link. Try again.";
+            res.redirect(`/users/resetpassword?status=error&message=${message}`);
+        }
+
+        const resetPassword = await PasswordReset.findOne({userId});
+
+        if(!resetPassword){
+            const message = "Invalid password reset link. Try again.";
+            res.redirect(`/users/resetpassword?status=error&message=${message}`);
+        }
+
+        const {expiresAt, token: resetToken} = resetPassword;
+
+        if(expiresAt < Date.now()) {
+            const message = "Reset Password link has expired. Please try again.";
+            res.redirect(`/users/resetpassword?status=error&message=${message}`);
+        }else{
+            const isMatch = await compareString(token, resetToken);
+
+            if(!isMatch){
+                const message = "Invalid reset password link. Please try again.";
+                res.redirect(`/users/resetpassword?status=error&message=${message}`);
+            }else{
+                res.redirect(`/users/resetpassword?type=reset&id=${userId}`);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const changePassword = async (req, res) => {
+    try {
+        const {userId, password} = req.body;
+
+        const hashedpassword = await hashString(password);
+
+        const user = await Users.findByIdAndUpdate(
+            {_id: userId},
+            {password: hashedpassword}
+            );
+
+            if(user){
+                await PasswordReset.findOneAndDelete({userId});
+
+                const message = "Password successfully reset.";
+                res.redirect(`/users/resetpassword?status=success&message=${message}`);
+                return;
+            }
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({message: error.message});
     }
 };

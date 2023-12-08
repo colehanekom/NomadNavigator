da79986c3cdf215714abd2895497de6b994317af
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-12-08 14:26:05 +0200

    Added getUser and updateUser routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
-import { changePassword, requestPasswordReset, resetPassword, verifyEmail } from "../controllers/userContoller.js";
+import { changePassword, getUser, requestPasswordReset, resetPassword, updateUser, verifyEmail } from "../controllers/userContoller.js";
+import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
@@ -10,6 +11,10 @@ router.get("/verify/:userId/:token", verifyEmail);
 router.post("/request-passwordreset", requestPasswordReset);
 router.get("/reset-password/:userId/:token", resetPassword);
 router.post("/reset-password", changePassword);
+
+//user routes
+router.post("/get-user/:id?", userAuth, getUser);
+router.put("/update-user", userAuth, updateUser);
 
 router.get("/verified", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/build", "index.html"));

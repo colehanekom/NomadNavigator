ebcfe76e1a56973e597afdf2dbde5db1510093c9
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-12-14 09:38:20 +0200

    Created postRoutes

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,12 @@
+// import express from "express";
+// import userAuth from "../middleware/authMiddleware.js";
+// import { createPost } from "../controllers/postController.js";
+
+// const router = express.Router();
+
+// //Create Post
+// router.post("/create-post", userAuth, createPost);
+// //Get Post
+// router.post("/", userAuth, getPosts);
+
+// export default router;

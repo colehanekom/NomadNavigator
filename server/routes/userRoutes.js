bd28ab0971df22240001016bb7c5806b2bd3ef55
colehanekom <Coleh304@gmail.com>
2024-01-07 17:41:45 +0000

    Updated userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path from "path";
-import { changePassword, getUser, requestPasswordReset, resetPassword, updateUser, verifyEmail } from "../controllers/userContoller.js";
+import { acceptRequest, changePassword, friendRequest, getFriendRequest, getUser, requestPasswordReset, resetPassword, suggestedFriends, updateUser, verifyEmail } from "../controllers/userContoller.js";
 import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -15,6 +15,16 @@ router.post("/reset-password", changePassword);
 //user routes
 router.post("/get-user/:id?", userAuth, getUser);
 router.put("/update-user", userAuth, updateUser);
+
+//friend requests
+router.post("/friend-request", userAuth, friendRequest);
+router.post("/get-friend-request", userAuth, getFriendRequest);
+
+//accept/ deny requests
+router.post("/accept-request", userAuth, acceptRequest);
+
+//suggested
+router.post("/suggested", userAuth, suggestedFriends);
 
 router.get("/verified", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/build", "index.html"));

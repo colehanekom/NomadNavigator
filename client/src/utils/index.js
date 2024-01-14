8395402fd33afd88ddb83c858416e69318f087c5
colehanekom <Coleh304@gmail.com>
2024-01-14 08:28:10 +0000

    Updated index.js

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -11,7 +11,7 @@ export const API = axios.create({
 export const apiRequest = async ({ url,token,data,method}) => {
     try {
         const result = await API(url, {
-            method:  method || "GET",
+            method: method || "GET",
             data: data,
             headers: {
                 "content-type": "application/json",
@@ -23,25 +23,25 @@ export const apiRequest = async ({ url,token,data,method}) => {
     } catch (error) {
         const err = error.response.data;
         console.log(err);
-        return{status: err.success, message: err.message};
+        return {status: err.success, message: err.message};
     }
 };
 
-export const handleFileUpload = async (uploadFile) => {
-   const formData = new FormData();
-    formData.append("file", uploadFile);
-    formData.append("upload_preset", "Nomad Navigator");
+export const handleFileUpload= async(uploadFile)=>{
+    const formData= new FormData();
+    formData.append("file",uploadFile);
+    formData.append("upload_preset","nomadnavigator");
 
-    try {
-        const response = await axios.post(
-            `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_ID}/image/upload/`,
-            formData
+    try{
+        const response= await axios.post(
+            `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_ID}/image/upload`,formData
         );
         return response.data.secure_url;
-    } catch (error) {
+    }catch(error){
         console.log(error);
     }
-}
+};
+
 
 export const fetchPosts = async (token, dispatch, uri, data) => {
     try {

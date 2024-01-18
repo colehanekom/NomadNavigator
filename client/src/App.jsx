5f7c9550e0bce20354871cdc5354379871bbb7fd
colehanekom <38474090+colehanekom@users.noreply.github.com>
2024-01-18 09:08:45 +0200

    Added route paths for ContentManagement, UserProfiles, ReportedIssues, Announcements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import {Outlet, Navigate, Routes, Route, useLocation} from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { Landing, Signin, Signup, Home, Profile, ResetPassword, EmailSignUp } from './pages';
-import {AdminDashboard, AdminLogin} from './admin';
+import {AdminDashboard, AdminLogin, Announcements, ContentManagement, ReportedIssues, UserProfiles} from './admin';
  
 function Layout(){
   const {user} = useSelector((state) => state.user);
@@ -30,7 +30,10 @@ function App() {
 
           <Route path="/admin" element={<AdminLogin />} />   
           <Route path="/admin-dashboard" element={<AdminDashboard />} />   
-
+          <Route path="/content-management" element={<ContentManagement />} />   
+          <Route path="/user-profiles" element={<UserProfiles />} />
+           <Route path="/reported-issues" element={<ReportedIssues />} /> 
+          <Route path="/announcements" element={<Announcements />} /> 
 
           <Route element={<Layout/>}>
           <Route path='/profile/:id?' element={<Profile/>} />

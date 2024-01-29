27dcb233ade47153814b082ee7e3d6191917201e
colehanekom <Coleh304@gmail.com>
2024-01-29 10:45:09 +0200

    Added more travel experiences

--- client/src/pages/TravelPage.jsx
+++ client/src/pages/TravelPage.jsx
@@ -9,6 +9,30 @@ const TravelPage = () => {
       lat: -33.918861,
       lng: 18.423300,
     },
+    {
+        title: 'Table Mountain Hike',
+        description: 'Breathtaking views from the top of Table Mountain',
+        lat: -33.9625,
+        lng: 18.4039,
+      },
+      {
+        title: 'Robben Island Tour',
+        description: 'Visiting the historic Robben Island',
+        lat: -33.8075,
+        lng: 18.3666,
+      },
+      {
+        title: 'Camps Bay Beach Day',
+        description: 'Relaxing on the sandy beaches of Camps Bay',
+        lat: -33.9518,
+        lng: 18.3783,
+      },
+      {
+        title: 'V&A Waterfront Shopping',
+        description: 'Exploring shops and restaurants at the V&A Waterfront',
+        lat: -33.9187,
+        lng: 18.4233,
+      },
   ];
 
   return (

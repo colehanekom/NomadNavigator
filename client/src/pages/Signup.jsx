0602df610604b0e8f8eeb1079986d8215bf6b8cb
colehanekom <Coleh304@gmail.com>
2024-02-09 09:06:45 +0200

    Updated heading text

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -55,7 +55,7 @@ const Signup = () => {
         {/* RIGHT */}
         <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>  
           <div className='w-full flex gap-2 items-center mb-6'>
-            <span className='text-4xl font-semibold'>Sign up with email</span>
+            <span className='text-4xl font-semibold'>Sign Up</span>
           </div>
 
           <form className="py-8 flex flex-col gap-5" 

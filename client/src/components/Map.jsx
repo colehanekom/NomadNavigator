88d0c2e0cd992bb0e5e4580f38e1a5f71054f23a
colehanekom <Coleh304@gmail.com>
2024-02-22 13:19:46 +0200

    Updated map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.mobile';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = ({ experiences }) => {
   const capeTownCenter = [-33.918861, 18.423300];
 
   return (
-    <MapContainer center={capeTownCenter} zoom={12} style={{ height: '500px', width: '100%' }}>
+    <MapContainer
+      center={capeTownCenter}
+      zoom={12}
+      style={{ height: '500px', width: '100%' }}
+      tap={false}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
